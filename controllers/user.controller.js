b4a6eef3984c2c5261cddcfde6388bfe10079bc6
deep7311 <deepakchoudhary73111@gmail.com>
2025-07-08 11:26:20 +0530

    updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,6 +12,7 @@ export const register = async (req, res) => {
       name,
       email,
       password: hashedpwd,
+      role: "user",
     };
     const result = await userModel.create(user);
     res.status(201).json(result);
