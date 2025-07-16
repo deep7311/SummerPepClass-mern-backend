be1a2a29b38a4f2e81ae700ef9e7d4bd87b31670
deep7311 <deepakchoudhary73111@gmail.com>
2025-07-16 12:02:42 +0530

    updated

--- server.js
+++ server.js
@@ -11,20 +11,20 @@ const app = express();
 const dbuser = encodeURIComponent(process.env.DB_USER);
 const dbpass = encodeURIComponent(process.env.DB_PASS);
 
-mongoose.connect("mongodb://localhost:27017/merncafe").then(() => {
-  console.log("Connected to MongoDB");
-  app.listen(8080, () => {
-    console.log("Server started at Port 8080");
-  });
-});
-
-
-// mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.0c8m8ik.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+// mongoose.connect("mongodb://localhost:27017/merncafe").then(() => {
 //   console.log("Connected to MongoDB");
 //   app.listen(8080, () => {
 //     console.log("Server started at Port 8080");
 //   });
 // });
+
+
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.0c8m8ik.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(8080, () => {
+    console.log("Server started at Port 8080");
+  });
+});
 
 app.use(cors())
 // app.use(cors({
